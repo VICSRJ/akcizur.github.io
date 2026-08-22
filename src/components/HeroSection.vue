c12df3b5e66068a6324704c546dcf46e4b253c72
vics <ruzickajakub95@gmail.com>
2026-08-22 15:33:07 +0200

    feat: choreograph hero motion layers

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,19 +1,19 @@
 <template>
   <section class="hero" id="top" data-motion-section>
-    <div class="container">
+    <div class="container" data-motion-preset="ParallaxDeep">
       <div class="hero-grid">
         <div>
-          <h1 data-motion="slide-left">RŮŽIČKA<br />JAKUB</h1>
-          <p data-motion="slide-up">
+          <h1 data-motion="slide-left" data-motion-preset="RevealCinematic">RŮŽIČKA<br />JAKUB</h1>
+          <p data-motion="slide-up" data-motion-preset="RiseFluid">
             Webový designer zaměřený na minimalistický přístup, čistou typografii a funkční design.
             Vytvářím digitální zážitky, které překvapují svou jednoduchostí.
           </p>
-          <div class="cta-buttons" data-motion="fade">
+          <div class="cta-buttons" data-motion="fade" data-motion-preset="GlideSmooth">
             <a href="#kontakt" class="btn"><span>ZAČÍT PROJEKT</span></a>
             <a href="#portfolio" class="btn"><span>PRÁCE</span></a>
           </div>
         </div>
-        <div class="hero-number" data-motion="slide-right">2025</div>
+        <div class="hero-number" data-motion="slide-right" data-motion-preset="DepthDeep">2025</div>
       </div>
     </div>
   </section>
