4c89cebcfe5a0e9b0a6dba642cfd69b84be3b5cb
vics <ruzickajakub95@gmail.com>
2026-08-22 19:26:20 +0200

    feat: load ZURB shadow layer

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import './styles/main.css'
 import './styles/remix-motion.css'
 import './styles/remix-final.css'
+import './styles/zurb-shadow.css'
 import './styles/mobile.css'
 
 createApp(App).mount('#app')
