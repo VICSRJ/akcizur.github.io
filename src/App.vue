9ae05d6fdd25d410f71cb4eaa9f7e14ea07accd0
vics <ruzickajakub95@gmail.com>
2026-08-22 18:00:21 +0200

    refactor: make Remix Motion the sole scroll runtime

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,6 @@ import ContactSection from './components/ContactSection.vue'
 import SiteFooter from './components/SiteFooter.vue'
 
 import { navLinks, aboutItems, services, portfolioItems } from './content/siteContent.js'
-import { useSmoothScroll } from './composables/useSmoothScroll.js'
 import { useRemixMotion } from './composables/useRemixMotion.js'
 
 const isMenuOpen = ref(false)
@@ -27,7 +26,6 @@ watch(isMenuOpen, (value) => {
   document.body.classList.toggle('menu-open', value)
 })
 
-useSmoothScroll()
 useRemixMotion()
 
 onMounted(() => {
