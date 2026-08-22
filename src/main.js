b07eb588517ee4e5935481136103eaaff1676d6b
vics <ruzickajakub95@gmail.com>
2026-08-22 20:01:02 +0200

    refactor: make Remix final motion stack authoritative

--- src/main.js
+++ src/main.js
@@ -2,12 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import './styles/main.css'
-import './styles/animations.css'
-import './styles/story.css'
-import './styles/motion.css'
-import './styles/about-story.css'
-import './styles/about-story-polish.css'
-import './styles/art-direction.css'
+import './styles/remix-motion.css'
+import './styles/remix-final.css'
 import './styles/mobile.css'
 
 createApp(App).mount('#app')
