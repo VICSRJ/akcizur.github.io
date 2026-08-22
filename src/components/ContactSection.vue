934b6c0736e3db213c2726bd811407b563f59fd9
vics <ruzickajakub95@gmail.com>
2026-08-22 15:33:22 +0200

    feat: choreograph contact climax motion

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -1,9 +1,9 @@
 <template>
   <section id="kontakt" class="contact" data-motion-section>
-    <div class="container">
-      <div class="section-label" data-motion="fade">POJĎME BUILDIT</div>
-      <h2 class="section-title" data-motion="slide-up">MÁTE<br />PROJEKT?</h2>
-      <!-- <a href="mailto:mail@ruzickajakub.cz" class="contact-email" data-motion="fade">mail@ruzickajakub.cz</a> -->
+    <div class="container" data-motion-preset="FocusDeep">
+      <div class="section-label" data-motion="fade" data-motion-preset="FadeMicro">POJĎME BUILDIT</div>
+      <h2 class="section-title" data-motion="slide-up" data-motion-preset="CurtainCinematic">MÁTE<br />PROJEKT?</h2>
+      <a href="mailto:mail@ruzickajakub.cz" class="contact-email" data-motion="fade" data-motion-preset="RevealCinematic">mail@ruzickajakub.cz</a>
     </div>
   </section>
 </template>
