69a5e3c2d30789d2b0c8db68f2c2b956c8d20e74
vics <ruzickajakub95@gmail.com>
2026-08-22 18:00:44 +0200

    refactor: make final Remix stack the only loaded motion layer

--- src/main.js
+++ src/main.js
@@ -2,17 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import './styles/main.css'
-import './styles/animations.css'
-import './styles/story.css'
-import './styles/motion.css'
-import './styles/about-story.css'
-import './styles/about-story-polish.css'
-import './styles/art-direction.css'
-import './styles/art-direction-refresh.css'
-import './styles/text-depth-shadow.css'
-import './styles/text-depth-shadow-v2.css'
-import './styles/scene-direction-v2.css'
 import './styles/remix-motion.css'
+import './styles/remix-final.css'
 import './styles/mobile.css'
 
 createApp(App).mount('#app')
