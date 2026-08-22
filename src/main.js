58cd5d8197bc2fe4a262bc258a9a9123b3a03b0a
vics <ruzickajakub95@gmail.com>
2026-08-22 16:09:45 +0200

    feat(motion): wire About story layer

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import './styles/main.css'
 import './styles/animations.css'
 import './styles/story.css'
 import './styles/motion.css'
+import './styles/about-story.css'
 import './styles/mobile.css'
 
 createApp(App).mount('#app')
